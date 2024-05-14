b75a10885a8524ab8ffb293542fd8b802ad3f585
Moonjunwoong <myc0611@naver.com>
2024-05-14 20:55:32 +0900

    feat : css 코드 및 js 코드 추가

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,7 +1,7 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
 
 const createTodo = function () {
-  const todoList = document.querySelector("#todo-list");
   const newLi = document.createElement("li");
   const newSpan = document.createElement("li");
   const newBtn = document.createElement("button");
@@ -10,11 +10,16 @@ const createTodo = function () {
     newLi.classList.toggle("complete");
   });
 
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+  });
+
   newSpan.textContent = todoInput.value;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
   todoInput.value = "";
+  saveItems();
 };
 
 // 항목 입력시 추가되는 코드
@@ -22,5 +27,23 @@ const keyCodeCheck = function () {
   // querySelector(태그 id).value는 id태그의 값을 가져온다.
   if (window.event.keyCode === 13 && todoInput.value !== "") {
     createTodo();
+  }
+};
+
+const deleteAll = function () {
+  const liList = document.querySelector("li");
+  for (let i = 0; i < liList.clientHeight; i++) {
+    liList[i].remove();
+  }
+};
+
+const saveItems = function () {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todo);
   }
 };
