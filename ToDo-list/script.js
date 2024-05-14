ae98ca30665262ba7cbf697b8ca64088f1bcd402
Moonjunwoong <myc0611@naver.com>
2024-05-14 19:04:37 +0900

    feat : css 코드 및 js 코드 추가

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,4 +1,26 @@
-const checkInput = function () {
+const todoInput = document.querySelector("#todo-input");
+
+const createTodo = function () {
+  const todoList = document.querySelector("#todo-list");
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("li");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+};
+
+// 항목 입력시 추가되는 코드
+const keyCodeCheck = function () {
   // querySelector(태그 id).value는 id태그의 값을 가져온다.
-  const inputValue = document.querySelector("#todo-input").value;
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
+    createTodo();
+  }
 };
